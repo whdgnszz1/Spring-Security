2221ad1e3c5697deb54157373dd10a87a6e6958d
홍종훈 <whdgnszz1@snu.ac.kr>
2024-06-25 23:33:24 +0900

    회원가입 API코드 작성

--- src/main/java/com/jh/security1/controller/IndexController.java
+++ src/main/java/com/jh/security1/controller/IndexController.java
@@ -1,34 +1,60 @@
 package com.jh.security1.controller;
 
+import com.jh.security1.config.auth.PrincipalDetails;
+import com.jh.security1.model.User;
+import com.jh.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Iterator;
 
 // View를 리턴하겠다.
 @Controller
 public class IndexController {
 
-    @GetMapping({"", "/"})
-    public String index() {
-        // 머스테치 기본폴더 src/main/resources
-        // 뷰리졸버 설정: templates(prefix), .mustache (suffix) 생략 가능!
-        return "index";
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({ "", "/" })
+    public @ResponseBody String index() {
+        return "인덱스 페이지입니다.";
     }
 
     @GetMapping("/user")
-    public String user() {
-        return "user";
-    }
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principal) {
+        System.out.println("Principal : " + principal);
+        System.out.println("OAuth2 : "+principal.getUser().getProvider());
+        // iterator 순차 출력 해보기
+        Iterator<? extends GrantedAuthority> iter = principal.getAuthorities().iterator();
+        while (iter.hasNext()) {
+            GrantedAuthority auth = iter.next();
+            System.out.println(auth.getAuthority());
+        }
 
+        return "유저 페이지입니다.";
+    }
 
     @GetMapping("/admin")
-    public String admin() {
-        return "admin";
+    public @ResponseBody String admin() {
+        return "어드민 페이지입니다.";
     }
 
+    //@PostAuthorize("hasRole('ROLE_MANAGER')")
+    //@PreAuthorize("hasRole('ROLE_MANAGER')")
+    @Secured("ROLE_MANAGER")
     @GetMapping("/manager")
-    public String manager() {
-        return "manager";
+    public @ResponseBody String manager() {
+        return "매니저 페이지입니다.";
     }
 
     @GetMapping("/login")
@@ -40,9 +66,15 @@ public class IndexController {
     public String join() {
         return "join";
     }
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc() {
-        return "회원가입 완료됨!";
+
+    @PostMapping("/joinProc")
+    public String joinProc(User user) {
+        System.out.println("회원가입 진행 : " + user);
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return "redirect:/";
     }
-    
 }
